554148135f3e1f492a97b4d2a444d0a252298e7e
cancellated <cancelation@qq.com>
2025-03-20 14:57:18 +0800

    test

---
+++ Assets/Scripts/Managers/SIngletonBase.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public abstract class SingletonBase<T> : MonoBehaviour where T : SingletonBase<T>
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+    private bool _isInitialized;
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        _instance = singleton.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+            if (!_isInitialized)
+            {
+                Initialize();
+                _isInitialized = true;
+            }
+        }
+    }
+
+    protected abstract void Initialize();
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+            _isInitialized = false;
+        }
+    }
+}
